2419e5bf8c91c9aaf5b2bed510e8fc5ba0d6a4a4
凯旋 - kǎixuán <51862296+KaiBelmo@users.noreply.github.com>
2026-06-16 21:00:15 +0100

    build(test-forms): add vite config

---
+++ apps/extension/test-forms/vite.config.ts
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: __dirname,
+  plugins: [react()],
+  base: "./",
+  build: {
+    outDir: "../dist-test-forms",
+    emptyOutDir: true,
+  },
+});
