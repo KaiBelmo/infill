37df357554284a48beb797391f0a263bf16169a0
凯旋 - kǎixuán <51862296+KaiBelmo@users.noreply.github.com>
2026-07-16 16:29:08 +0100

    fix(extension): add firefox manifest metadata

--- apps/extension/manifest.config.ts
+++ apps/extension/manifest.config.ts
@@ -10,20 +10,21 @@ export default defineManifest({
   host_permissions: ["file:///*", "https://*/*", "http://*/*"],
   background: {
     service_worker: "src/background/serviceWorker.ts",
+    scripts: ["src/background/serviceWorker.ts"],
     type: "module"
   },
   icons: {
-    "16": "public/infill-icon.png",
-    "48": "public/infill-icon.png",
-    "128": "public/infill-icon.png"
+    "16": "public/icons/infill-16.png",
+    "48": "public/icons/infill-48.png",
+    "128": "public/icons/infill-128.png"
   },
   action: {
     default_title: "Open Infill",
     default_popup: "src/popup/index.html",
     default_icon: {
-      "16": "public/infill-icon.png",
-      "48": "public/infill-icon.png",
-      "128": "public/infill-icon.png"
+      "16": "public/icons/infill-16.png",
+      "48": "public/icons/infill-48.png",
+      "128": "public/icons/infill-128.png"
     }
   },
   content_scripts: [
@@ -33,5 +34,13 @@ export default defineManifest({
       run_at: "document_idle"
     }
   ],
-  options_page: "src/options/index.html"
+  options_page: "src/options/index.html",
+  browser_specific_settings: {
+    gecko: {
+      id: "infill-extension@kaibelmo.com",
+      data_collection_permissions: {
+        required: ["none"]
+      }
+    }
+  }
 });
